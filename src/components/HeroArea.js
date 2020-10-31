de93c3f709586f40c2b20e90f32a49e8b523d937
ringkurahman <mrringkurahman@gmail.com>
2020-10-31 18:44:17 +0400

    modal fix

--- src/components/HeroArea.js
+++ src/components/HeroArea.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import Typical from 'react-typical'
 import { useSpring, animated } from 'react-spring'
-import { Button, Col, Container, Row, Image } from 'react-bootstrap'
+import { Button, Col, Container, Row, Image, Form, Modal } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import myImage from '../images/ringku-rahman.png'
-import ModalForm from './ModalForm'
+import emailjs from 'emailjs-com'
+import Recaptcha from 'react-recaptcha'
+import ModalFormContainer from './ModalFormContainer'
 
     
     // Image animation
@@ -24,10 +26,63 @@ const HeroArea = () => {
     const [props, met] = useSpring(() => ({ xy: [0, 0] }))
 
     // Modal state and function
-        const [show, setShow] = useState(false);
+        const [show, setShow] = useState(false)
 
-        const handleClose = () => setShow(false);
-        const handleShow = () => setShow(true);
+        const handleClose = () => setShow(false)
+    const handleShow = () => setShow(true)
+    
+    // Modal sms state
+    const [success, setSuccess] = useState('')
+    const [fail, setFail] = useState('')
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+
+        emailjs.sendForm('resume', 'template_3pvj7hq', e.target, 'user_toV7VpsuQCYFYlN1UxWA1')
+            .then((result) => {
+            setSuccess(result)
+            console.log(result.text);
+            }, (error) => {
+            setFail(error)
+            console.log(error.text);
+        })
+        e.target.reset()
+    }
+
+    // Multiple argument state
+    const [user, setUser] = useState({
+        name: '',
+        email: '',
+        password: '',
+        message: '',
+    })
+    
+    // Form field validation
+    const handleBlur = (e) => {
+        let isFieldValid;
+        if (e.target.name === 'name') {
+            isFieldValid = e.target.value
+        }
+        if (e.target.name === 'email') {
+            // validation email
+            isFieldValid = /\S+@\S+\.\S+/.test(e.target.value);
+        }
+        if (e.target.name === 'password') {
+            // validation 1 uppercase 1 lowercase 1 number
+            isFieldValid = /^(?=(?:.*[A-Z]){1})(?=(?:.*[a-z]){1})(?=(?:.*[0-9]){1})\S{6,20}$/.test(
+                e.target.value
+            );
+        }
+        if (isFieldValid) {
+        const newUserInfo = { ...user }
+        newUserInfo[e.target.name] = e.target.value
+        setUser(newUserInfo)
+        }
+    }
+
+    const recaptchaLoaded = () => {
+        console.log('recaptcha loaded')
+    }
 
     return (
         <section className='hero-area' id='home'>
@@ -57,7 +112,57 @@ const HeroArea = () => {
                         <hr />
                         <a className='btn btn-secondary mr-3' href='https://drive.google.com/uc?export=download&id=1X1UkdI5VCoks15CzefgHyDHG59tyWyJF' target="_blank" rel="no-referrer">Download Resume</a>
                         <Button onClick={handleShow}>Send Message</Button>
-                        <ModalForm handleShow={handleShow} handleClose={handleClose} show={show}></ModalForm>
+                        
+                        <Modal show={show} onHide={handleClose}>
+                            <Modal.Header closeButton>
+                            <Modal.Title>Get In Touch</Modal.Title>
+                            </Modal.Header>
+                                    <Modal.Body>
+                                        <ModalFormContainer>
+                                            <Form onSubmit={sendEmail}>
+                                                {success && <p className='text-success text-center'>Message Send Successfully</p>}
+                                                {fail && <p className='text-danger text-center'>Message Failed</p>}
+                                                <label>Your Name</label>
+                                                <input
+                                                    className='form-control mb-3'
+                                                    type="text"
+                                                    name="name"
+                                                    onBlur={handleBlur}
+                                                    placeholder='Name' required />
+                                                <label>Email Address</label>
+                                                <input
+                                                    className='form-control mb-3'
+                                                    type="email"
+                                                    name="email"
+                                                    onBlur={handleBlur}
+                                                    placeholder='Email' required />
+                                                <label>Subject</label>
+                                                <input
+                                                    className='form-control mb-3'
+                                                    type="text"
+                                                    name="subject"
+                                                    onBlur={handleBlur}
+                                                    placeholder='Subject' required />
+                                                <label>Message</label>
+                                                <textarea
+                                                    className='form-control'
+                                                    name="message"
+                                                    onBlur={handleBlur}
+                                                    rows="3" required />
+                                                <div className='mt-4'>
+                                                <Button variant="secondary" onClick={handleClose}>Close</Button>
+                                                <Button type='submit' variant='primary' value='send'>Submit</Button>
+                                                </div>
+                                                <Recaptcha
+                                                    sitekey="6Lcr-twZAAAAAC6eUnqjI8hoS8CE_fOn8nupgtYh"
+                                                    render="explicit"
+                                                    onloadCallback={recaptchaLoaded}
+                                                />
+                                            </Form>
+                                        </ModalFormContainer>
+                                    </Modal.Body>
+                            </Modal>
+
                     </Col>
                     <Col xs={12} sm={12} md={6}>
                         <animated.div
